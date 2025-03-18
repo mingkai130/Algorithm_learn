59ddc0e18b8535ecab63676657603e3e50f045ef
mingkai130 <mingkai130@1248070230@qq.com>
2025-03-18 20:20:36 +0800

    3/18 xiaomi.com

--- src/main/java/算法/Solution.java
+++ src/main/java/算法/Solution.java
@@ -4,6 +4,7 @@ import javax.imageio.event.IIOReadProgressListener;
 import javax.sound.sampled.ReverbType;
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicTreeUI;
+import javax.swing.text.EditorKit;
 import javax.swing.text.Element;
 import javax.swing.text.StyledEditorKit;
 import javax.xml.stream.FactoryConfigurationError;
@@ -18,9 +19,11 @@ import java.math.BigInteger;
 import java.text.StringCharacterIterator;
 import java.time.Instant;
 import java.time.chrono.IsoChronology;
+import java.time.temporal.Temporal;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.function.BiConsumer;
 import java.util.logging.Level;
 
 import 算法.数据结构.*;
@@ -31,18 +34,39 @@ class Solution {
 
     public static void main(String[] args) {
         int[][] ints = new int[][]{{1,3,1},{1,5,1},{4,2,1}};
-        int[] ints2 = new int[]{8,9,9,9};
+        int[] ints2 = new int[]{1,2,3};
+        int[][] booleanInts = new int[][]{{0,1,1, 0},{3,4,5,6},{1,3,1,5}};
 //        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
 
 //        ListNode head = new ListNode(1);
 //        head.add(2);head.add(3);head.add(4);head.add(5);
 
-        System.out.println();
+
+        System.out.println(subsets(ints2));
     }
 
-    // 72. 编辑距离
-    public int minDistance(String word1, String word2) {
+    // 78. 子集   给一个 int[]，返回所有可能的子集   这里答案用 dfs！太牛逼了，index 表示下标位置   哎好巧明，自己想不到
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
-        return 0;
+        dfs(ans, temp, nums, 0);
+
+        return ans;
+    }
+    public static void dfs(List<List<Integer>> ans, List<Integer> temp, int[] nums, int index){
+        // index 走到最后一位
+        if (index == nums.length) ans.add(new ArrayList<>(temp));
+        // 中间位置只分为选择和不选择当前数字
+        else {
+            // 选择
+            temp.add(nums[index]);
+            dfs(ans, temp, nums, index + 1);
+            temp.removeLast();
+            // 不选择
+            dfs(ans, temp, nums, index + 1);
+        }
     }
 }
+
+
--- src/main/java/算法/finished_codes3.java
+++ src/main/java/算法/finished_codes3.java
@@ -1,9 +1,6 @@
 package 算法;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Objects;
-import java.util.Stack;
+import java.util.*;
 
 public class finished_codes3 {
 
@@ -169,4 +166,165 @@ public class finished_codes3 {
         }
         return sb.isEmpty() ? "/" : sb.toString();
     }
+
+    // 72. 编辑距离 不会，答案用动态规划  注意初始化！
+    public static int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        if (word1.isEmpty() || word2.isEmpty()) return word1.length() + word2.length();
+        // 初始化
+        dp[0][0] = 0;
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length() ; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],Math.min(dp[i - 1][j],dp[i][j - 1])) + 1;
+                }
+            }
+        }
+
+
+        return dp[word1.length()][word2.length()];
+    }
+
+    // 73. 矩阵置零  自己暴力模拟，虽然能过但是打败 1% 同行，复杂度有点高
+    public static void setZeroes(int[][] matrix) {
+        HashSet<List<Integer>> hs = new HashSet<>();
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 矩阵中 0 的位置入 Set 集合
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    List<Integer> temp = new ArrayList<>();temp.add(i);temp.add(j);
+                    hs.add(temp);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                List<Integer> temp = new ArrayList<>();temp.add(i);temp.add(j);
+                // 如果这个位置是 0, 处理横竖
+                if (hs.contains(temp)){
+                    // 处理行
+                    for (int k = 0; k < n; k++) {
+                        matrix[i][k] = 0;
+                    }
+                    // 处理列
+                    for (int k = 0; k < m; k++) {
+                        matrix[k][j] = 0;
+                    }
+                }
+            }
+
+        }
+    }
+
+    // 74. 搜索二维矩阵  暴力模拟，二分查找秒了
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int line = -1;
+        // 先找行
+        for (int i = 0; i < m; i++) {
+            if (target >= matrix[i][0] && target <= matrix[i][n - 1])
+                line = i;
+        }
+        if (line == -1)
+            return false;
+        // 行内二分查找
+        int left = 0, right = n - 1;
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if (matrix[line][mid] == target) return true;
+            else if (target > matrix[line][mid]){
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+
+    // 75. 颜色分类   荷兰三色旗问题  乱序数组最终为 0,0,0,0,1,1,1,1,2,2,2  这个题太细节了
+    // 如果遇到0，不能简单地换位置，当 P0 < P1,如果简单交换，可能会把第一个1交换出去，导致答案错误
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                ++p1;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                ++p0;
+                ++p1;
+            }
+        }
+    }
+
+
+    // 77.组合  一眼回溯，这里忘的差不多了   磕磕绊绊弄好了，但是还是不知道为啥通过
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        backTrace(ans, temp, n, k, 1);
+
+        return ans;
+    }
+    public static void backTrace(List<List<Integer>> ans, List<Integer> current, int n, int k, int cur) {
+        // 答案加入集合
+        if (current.size() == k) ans.add(new ArrayList<>(current));
+
+            // 中间层
+        else {
+            for (int i = (current.isEmpty()?0:current.getLast()) + 1; i <= n; i++) {
+                current.add(i);
+                backTrace(ans, current, n, k, cur + 1);
+                current.removeLast();
+            }
+
+
+        }
+    }
+
+
+    // 78. 子集   给一个 int[]，返回所有可能的子集   这里答案用 dfs！太牛逼了，index 表示下标位置   哎好巧明，自己想不到
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        dfs(ans, temp, nums, 0);
+
+        return ans;
+    }
+    public static void dfs(List<List<Integer>> ans, List<Integer> temp, int[] nums, int index){
+        // index 走到最后一位
+        if (index == nums.length) ans.add(new ArrayList<>(temp));
+            // 中间位置只分为选择和不选择当前数字
+        else {
+            // 选择
+            temp.add(nums[index]);
+            dfs(ans, temp, nums, index + 1);
+            temp.removeLast();
+            // 不选择
+            dfs(ans, temp, nums, index + 1);
+        }
+    }
+
 }
